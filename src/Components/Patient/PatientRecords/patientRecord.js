c7262f6c8e425eaaad49c118078c32451da834fd
AliRaza675 <razaa0300@gmail.com>
2023-12-02 01:04:36 +0500

    updated

--- src/Components/Patient/PatientRecords/patientRecord.js
+++ src/Components/Patient/PatientRecords/patientRecord.js
@@ -1,12 +1,144 @@
-import React from "react";
-import "../PatientRecords/patientRecrodStyle.css";
-const PatientRecrod = () => {
+import React from 'react'
+import '../PatientRecords/patientRecrodStyle.css'
+import Profile from '../../../Images/doctor svg.svg'
+
+function patientRecord() {
   return (
-    <div className="main">
-      <div className="innerDiv">
-        <h2>Patient Record</h2>
+
+    <div className='main-section'>
+      <h3>Patient Mangement</h3>
+
+
+    <div className='main-card'>
+      <div className='user-info'>
+        <div className='user-pic'>
+
+          <img src={Profile} alt=''/>
+          
+
+        </div>
+        <div className='user-name'>
+        <p className='name'>John Doe</p>
+          <p className='email'>john.doe@mail.com</p>
+
+        </div>
+      </div>
+
+
+      <div className='buttons'>
+        <button className='delete'>Delete Record</button>
+        <button className='active'>Active/Deactive</button>
+        <button className='view-profile'>View Profile</button>
+
       </div>
     </div>
-  );
-};
-export default PatientRecrod;
+
+
+    <div className='main-card'>
+      <div className='user-info'>
+        <div className='user-pic'>
+
+          <img src={Profile} alt=''/>
+          
+
+        </div>
+        <div className='user-name'>
+        <p className='name'>John Doe</p>
+          <p className='email'>john.doe@mail.com</p>
+
+        </div>
+      </div>
+
+
+      <div className='buttons'>
+        <button className='delete'>Delete Record</button>
+        <button className='active'>Active/Deactive</button>
+        <button className='view-profile'>View Profile</button>
+
+      </div>
+    </div>
+
+
+    <div className='main-card'>
+      <div className='user-info'>
+        <div className='user-pic'>
+
+          <img src={Profile} alt=''/>
+          
+
+        </div>
+        <div className='user-name'>
+        <p className='name'>John Doe</p>
+          <p className='email'>john.doe@mail.com</p>
+
+        </div>
+      </div>
+
+
+      <div className='buttons'>
+        <button className='delete'>Delete Record</button>
+        <button className='active'>Active/Deactive</button>
+        <button className='view-profile'>View Profile</button>
+
+      </div>
+    </div>
+
+
+    <div className='main-card'>
+      <div className='user-info'>
+        <div className='user-pic'>
+
+          <img src={Profile} alt=''/>
+          
+
+        </div>
+        <div className='user-name'>
+        <p className='name'>John Doe</p>
+          <p className='email'>john.doe@mail.com</p>
+
+        </div>
+      </div>
+
+
+      <div className='buttons'>
+        <button className='delete'>Delete Record</button>
+        <button className='active'>Active/Deactive</button>
+        <button className='view-profile'>View Profile</button>
+
+      </div>
+    </div>
+
+
+    <div className='main-card'>
+      <div className='user-info'>
+        <div className='user-pic'>
+
+          <img src={Profile} alt=''/>
+          
+
+        </div>
+        <div className='user-name'>
+        <p className='name'>John Doe</p>
+          <p className='email'>john.doe@mail.com</p>
+
+        </div>
+      </div>
+
+
+      <div className='buttons'>
+        <button className='delete'>Delete Record</button>
+        <button className='active'>Active/Deactive</button>
+        <button className='view-profile'>View Profile</button>
+
+      </div>
+    </div>
+
+    </div>
+
+
+
+
+    )
+}
+
+export default patientRecord
